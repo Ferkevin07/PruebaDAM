59bf1fc84f1bebd4c7249e33cfa3722deff92331
Kevin Benavides <66317435+Ferkevin07@users.noreply.github.com>
2022-07-18 21:04:15 -0500

    Deleted credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,26 +4,16 @@
 
 export const environment = {
   firebase: {
-    projectId: 'project-bim1',
-    appId: '1:127096356098:web:5cc8e7690ee36803ccd61b',
-    storageBucket: 'project-bim1.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyB5JIXKcsdRdWBtciqLA1y_hPfLfMd1EQY',
-    authDomain: 'project-bim1.firebaseapp.com',
-    messagingSenderId: '127096356098',
-    measurementId: 'G-0ZJRJES9RJ',
+    projectId: '**',
+    appId: '**',
+    storageBucket: '**',
+    locationId: '**',
+    apiKey: '**',
+    authDomain: '**',
+    messagingSenderId: '**',
+    measurementId: '**',
   },
   production: false,
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  firebaseConfig : {
-  apiKey: 'AIzaSyB5JIXKcsdRdWBtciqLA1y_hPfLfMd1EQY',
-  authDomain: 'project-bim1.firebaseapp.com',
-  projectId: 'project-bim1',
-  storageBucket: 'project-bim1.appspot.com',
-  messagingSenderId: '127096356098',
-  appId: '1:127096356098:web:5cc8e7690ee36803ccd61b',
-  measurementId: 'G-0ZJRJES9RJ',
-  },
 };
 /*
  * For easier debugging in development mode, you can import the following file
